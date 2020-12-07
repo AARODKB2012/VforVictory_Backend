f04bef4dcc7ecc4340966b66c45554062f8f054d
AngelKBR <75036704+AngelKBR@users.noreply.github.com>
2020-12-06 20:50:06 -0500

    updated
    added nodemon for NPM start script, updated businesses

--- app.js
+++ app.js
@@ -10,26 +10,21 @@ app.use(bodyParser.json());
 
 // Allowing access to app resources from other servers.
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  next();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+    next();
 });
 
 
-app.use("/api",routes);
+app.use("/api", routes);
 
 
 app.listen(port, function (err) {
-  if (err)
-    throw err;
-  console.log('Your server is running on port: ' + port + '.');
+    if (err) 
+        throw err;
+    
+    console.log('Your server is running on port: ' + port + '.');
 });
 
 module.exports = app;
--- v1/controllers/main.js
+++ v1/controllers/main.js
@@ -1,79 +1,79 @@
-const sql = require('../../model/entity');
+const sql = require("../../model/entity");
 
-exports.getNotFound = function (req, res,next){
+exports.getNotFound = function (req, res, next) {
     res.status(404).send();
-}
+};
 
-exports.getHelloWorld = function (req, res,next){
-    res.status(200).send('Hello World');
-}
+exports.getHelloWorld = function (req, res, next) {
+    res.status(200).send("Hello World");
+};
 
-exports.getAllVolunteers = async function (req, res,next){
+exports.getAllVolunteers = async function (req, res, next) {
     let volunteerList = [];
     volunteerList = await sql.getAllVolunteers();
-    if(volunteerList.length > 0){
-        res.status(200).json({status:200, results: volunteerList, resultsLength: volunteerList.length});
-    }else{
+    if (volunteerList.length > 0) {
+        res.status(200).json({status: 200, results: volunteerList, resultsLength: volunteerList.length});
+    } else {
         res.status(204).send();
     }
-}
+};
 
-exports.getVolunteerByUserNameAndPassword = async function (req, res,next){
+exports.getVolunteerByUserNameAndPassword = async function (req, res, next) {
     let volunteerList = [];
-    volunteerList = await sql.getVolunteerByUserNameAndPassword(req.params.userName,req.params.password);
-    if(volunteerList.length > 0){
-        res.status(200).json({status:200, results: volunteerList, resultsLength: volunteerList.length});
-    }else{
+    volunteerList = await sql.getVolunteerByUserNameAndPassword(req.params.userName, req.params.password);
+    if (volunteerList.length > 0) {
+        res.status(200).json({status: 200, results: volunteerList, resultsLength: volunteerList.length});
+    } else {
         res.status(204).send();
     }
-}
+};
 
-exports.createNewVolunteer = async function (req, res,next){
-  let rowCount = sql.createNewVolunteer(req.body);
-  console.log(rowCount);
-  if(rowCount == 1){
-    res.status(201).json({userCreated: true});
-  }else{
-    res.status(202).send();
-  }
-}
+exports.createNewVolunteer = async function (req, res, next) {
+    let rowCount = sql.createNewVolunteer(req.body);
+    console.log(rowCount);
+    if (rowCount == 1) {
+        res.status(201).json({userCreated: true});
+    } else {
+        res.status(202).send();
+    }
+};
 
-exports.getAllVolunteersByStatus = async function (req, res,next){
-  let volunteerList = [];
-  volunteerList = await sql.getAllVolunteersByStatus(req.params.status);
-  if(volunteerList.length > 0){
-      res.status(200).json({status:200, results: volunteerList, resultsLength: volunteerList.length});
-  }else{
-      res.status(204).send();
-  }
-}
+exports.getAllVolunteersByStatus = async function (req, res, next) {
+    let volunteerList = [];
+    volunteerList = await sql.getAllVolunteersByStatus(req.params.status);
+    if (volunteerList.length > 0) {
+        res.status(200).json({status: 200, results: volunteerList, resultsLength: volunteerList.length});
+    } else {
+        res.status(204).send();
+    }
+};
 
-exports.getAllVolunteersBySearchValue = async function (req, res,next){
-  let volunteerList = [];
-  volunteerList = await sql.getAllVolunteersBySearchValue(req.params.searchValue);
-  if(volunteerList.length > 0){
-      res.status(200).json({status:200, results: volunteerList, resultsLength: volunteerList.length});
-  }else{
-      res.status(204).send();
-  }
-}
+exports.getAllVolunteersBySearchValue = async function (req, res, next) {
+    let volunteerList = [];
+    volunteerList = await sql.getAllVolunteersBySearchValue(req.params.searchValue);
+    if (volunteerList.length > 0) {
+        res.status(200).json({status: 200, results: volunteerList, resultsLength: volunteerList.length});
+    } else {
+        res.status(204).send();
+    }
+};
 
-exports.getAllBusinesses = async function (req, res,next){
+exports.getAllBusinesses = async function (req, res, next) {
     let businessesArray = [];
-   businessesArray = await sql.getAllBusinesses();
-    if(businessesArray.length > 0){
-        res.status(200).json({status:200, results:businessesArray, resultsLength:businessesArray.length});
-    }else{
+    businessesArray = await sql.getAllBusinesses();
+    if (businessesArray.length > 0) {
+        res.status(200).json({status: 200, results: businessesArray, resultsLength: businessesArray.length});
+    } else {
         res.status(204).send();
     }
-}
+};
 
-exports.createNewBusiness = async function (req, res,next){
-  let rowCount = JSON.stringify(sql.createNewBusiness(req.body));
-  console.log(rowCount);
-  if(rowCount == 1){
-    res.status(201).json({businessObject: true});
-  }else{
-    res.status(202).send();
-  }
-}
+exports.createNewBusiness = async function (req, res, next) {
+    let rowCount = JSON.stringify(sql.createNewBusiness(req.body));
+    console.log(rowCount);
+    if (rowCount == 1) {
+        res.status(201).json({businessObject: true});
+    } else {
+        res.status(202).send();
+    }
+};
