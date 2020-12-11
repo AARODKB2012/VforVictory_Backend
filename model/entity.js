9065b3d512d5f296aae97b7fb90bb985228be23d
AngelKBR <75036704+AngelKBR@users.noreply.github.com>
2020-12-10 20:00:08 -0500

    update

--- model/entity.js
+++ model/entity.js
@@ -1,4 +1,4 @@
-var Connection = require("tedious").Connection;
+var Connection = require('tedious').Connection;
 var ConnectionPool = require("tedious-connection-pool");
 var Request = require("tedious").Request;
 var TYPES = require("tedious").TYPES;
